3ed4fedad459e99822a1cb8f1ee34b7362e1679b
Morgan Creekmore <morgan@creekmore.dev>
2025-01-12 01:19:29 -0600

    Setup event proxies

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 #![allow(unused)]
 
 use tao::event::{Event, StartCause, WindowEvent};
-use tao::event_loop::{ControlFlow, EventLoop};
+use tao::event_loop::{ControlFlow, EventLoop, EventLoopBuilder};
 use tao::window::WindowBuilder;
 use tray_icon::{
     menu::{AboutMetadata, Menu, MenuEvent, MenuItem, PredefinedMenuItem},
@@ -15,9 +15,25 @@ use rust_embed::Embed;
 #[folder = "icons/"]
 struct Asset;
 
+#[derive(Debug)]
+enum UserEvent {
+    TrayIconEvent(TrayIconEvent),
+    MenuEvent(MenuEvent)
+}
+
 fn main() -> wry::Result<()> {
-    let event_loop = EventLoop::new();
-    event_loop.set_device_event_filter(tao::event_loop::DeviceEventFilter::Never);
+    let mut event_loop_builder = EventLoopBuilder::<UserEvent>::with_user_event();
+    let event_loop = event_loop_builder.build();
+
+    // set a menu event handler that wakes up the event loop
+    let proxy = event_loop.create_proxy();
+    TrayIconEvent::set_event_handler(Some(move |event| {
+        let _ = proxy.send_event(UserEvent::TrayIconEvent(event));
+    }));
+    let proxy = event_loop.create_proxy();
+    MenuEvent::set_event_handler(Some(move |event| {
+        let _ = proxy.send_event(UserEvent::MenuEvent(event));
+    }));
 
     let window = WindowBuilder::new()
         .with_title("Resilio Sync")
@@ -43,49 +59,40 @@ fn main() -> wry::Result<()> {
 
     let mut tray_icon = None;
 
-    let menu_channel = MenuEvent::receiver();
-    let tray_channel = TrayIconEvent::receiver();
-
     event_loop.run(move |event, _, control_flow| {
-        // We add delay of 16 ms (60fps) to event_loop to reduce cpu load.
-        // This can be removed to allow ControlFlow::Poll to poll on each cpu cycle
-        // Alternatively, you can set ControlFlow::Wait or use TrayIconEvent::set_event_handler,
-        // see https://github.com/tauri-apps/tray-icon/issues/83#issuecomment-1697773065
-        *control_flow = ControlFlow::WaitUntil(
-            std::time::Instant::now() + std::time::Duration::from_millis(16),
-        );
+        *control_flow = ControlFlow::Wait;
 
-        if let Event::NewEvents(StartCause::Init) = event {
-            let icon = load_icon();
+        match event {
+            Event::NewEvents(tao::event::StartCause::Init) => {
+                let icon = load_icon();
 
-            // We create the icon once the event loop is actually running
-            // to prevent issues like https://github.com/tauri-apps/tray-icon/issues/90
-            tray_icon = Some(
-                TrayIconBuilder::new()
-                    .with_menu(Box::new(tray_menu.clone()))
-                    .with_tooltip("Resilio Sync") //TODO update tooltip and icon based on service status
-                    .with_icon(icon)
-                    .build()
-                    .unwrap(),
-            );
-        }
-
-        if let Ok(event) = menu_channel.try_recv() {
-            if event.id == open.id() {
-                window.set_visible(true);
-            } else if event.id == quit.id() {
-                tray_icon.take();
-                *control_flow = ControlFlow::Exit;
+                // We create the icon once the event loop is actually running
+                // to prevent issues like https://github.com/tauri-apps/tray-icon/issues/90
+                tray_icon = Some(
+                    TrayIconBuilder::new()
+                        .with_menu(Box::new(tray_menu.clone()))
+                        .with_tooltip("Resilio Sync") //TODO update tooltip and icon based on service status
+                        .with_icon(icon)
+                        .build()
+                        .unwrap(),
+                );
             }
-        }
-
-        if let Ok(event) = tray_channel.try_recv() {
-            //Doesnt seem to do anything?
-        }
-
-        //Minimize to tray instead of closing
-        if let Event::WindowEvent { window_id, event: WindowEvent::CloseRequested, .. } = event {
-            window.set_visible(false);
+            Event::UserEvent(UserEvent::MenuEvent(event)) => {
+                if event.id == open.id() {
+                    window.set_visible(true);
+                } else if event.id == quit.id() {
+                    tray_icon.take();
+                    *control_flow = ControlFlow::Exit;
+                }
+            }
+            Event::UserEvent(UserEvent::TrayIconEvent(event)) => {
+                //Doesnt seem to do anything?
+            }
+            //Minimize to tray instead of closing
+            Event::WindowEvent { window_id, event: WindowEvent::CloseRequested, .. } => {
+                window.set_visible(false);
+            }
+            _ => {}
         }
     })
 }
